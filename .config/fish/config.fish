f81e0d936f74e41f788322bae57dec8c5ee64d08
Bruno Lima <brunoluislima19@gmail.com>
2024-01-08 23:38:56 -0300

    asdf source activate

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,9 @@ command -qv nvim && alias vim nvim
 # theme
 oh-my-posh init fish --config ~/.config/oh-my-posh/themes/catppuccin_macchiato.omp.json | source
 
+# asdf
+source /opt/asdf-vm/asdf.fish
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
